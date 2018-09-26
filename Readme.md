9623785fc62f7f747a4df0623673e245aaf68b0c
Oriol Tauleria <oriol.tauleria@capside.com>
2018-09-26 23:10:17 +0200

    Test commit

--- Readme.md
+++ Readme.md
@@ -1,64 +1,30 @@
 # No-intro romset selector
 
 Tired of getting thousands of repeated games with USA, Europe, Asia ... versions and Rev 1, Alphas, etc?
-This app will help you to select only desired games.
+This app will help you to select only the desired games.
 
 ## Getting Started
 
-These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
-
+git clone the repo and
 ### Prerequisites
 
-This works with python3
-
-```
-Give examples
-```
+Download the RomSet and uncompress in a folder. Use this folder as a origin folder.
+Create a new folder (where desired games will be copied). Use this folder as destination folder.
 
 ### Installing
 
-A step by step series of examples that tell you how to get a development env running
-
-Say what the step will be
+Copy the script and launch using python3
 
 ```
-Give the example
+python3 romselector.py origin-folder destination-folder
 ```
-
-And repeat
-
-```
-until finished
-```
-
-End with an example of getting some data out of the system or using it for a little demo
 
 ## Running the tests
 
-Explain how to run the automated tests for this system
-
-### Break down into end to end tests
-
-Explain what these tests test and why
-
-```
-Give an example
-```
-
-### And coding style tests
-
-Explain what these tests test and why
-
-```
-Give an example
-```
-
-## Deployment
-
-Add additional notes about how to deploy this on a live system
+TO DO
 
 ## Built With
-
+l
 * Python
 
 ## Contributing
@@ -70,6 +36,10 @@ Create your pull request!
 * **Oriol Tauleria** - [uritau](https://github.com/uritau)
 
 ## To Do
-* Create interactive selection, for each "tag" ask if the user would to add the games uder the tag.
-* Create the function to "copy"
-* After the selection ask user about "non-copied unique games". To clarify: A "unique game" is a game without versions or distribution zones.
+* Create the "copy" function 
+* Check if the parameters are correct
+* Update the flow: 
+    * Ask if would to copy only games under certain tags, all unique games or all of them.
+    * If only certain tags: follow the current flow.
+    * All of them: Copy all the games
+    * All unique games: Ask about order preference (USA / EUROPE /JAPAN ) and ask about special tags (beta, Unl, Rev A, Rev X, etc. )
--- romselector.py
+++ romselector.py
@@ -3,12 +3,11 @@ from os.path import isfile, join
 import sys
 import re
 
-def read_work_dir( roms_dir ):
-    return roms_dir
 
 def read_params():
-    work_dir = read_work_dir(sys.argv[1])
-    return work_dir
+    work_dir = sys.argv[1]
+    dest_dir = sys.argv[2]
+    return work_dir, dest_dir
 
 def list_files(work_dir):
     roms_list = [f for f in listdir(work_dir) if isfile(join(work_dir, f))]
@@ -41,6 +40,11 @@ def list_tags(roms_list):
         for i in range (2,len(rom)):
             if rom[i] not in tag_list:
                 tag_list.append(rom[i])
+    if not len(tag_list):
+        print("\n########################################################## WARNING ######################################################")
+        print("There are no files in the origin folder that follows the no_intro naming, confirm the folder is correct and launch again.")
+        print("#########################################################################################################################\n\n")
+        exit()
     return tag_list
 
 def count_games_tag(tag,roms):
@@ -54,13 +58,30 @@ def count_games_tags(tag_list,roms):
     for tag in tag_list:
         tag_count = count_games_tag(tag,roms)
 
-def message_welcome():
-    print ("Start program")
+def welcome_message():
+    print ("Starting Rom Selector, welcome ^^!!")
 
+def choose_selection_mode():
+    print ("You can choose the easy selector, that will import all games but only one version (European or USA or Japanese or others)\
+and will let you choose only if you would to import Beta games ")
+    print ("Or you can choose the advanced selector, that will let you select which game tags will be imported.")
+    answer = ""
+    answer = input("What do you preffer?\nEasy (E) or\nAdvanced (A)?\n")
+    if (answer == 'A') | (answer == 'a') | (answer == 'Advanced') | (answer == 'ADVANCED') | (answer == 'Advanced (A)'):
+        print ("You have selected the Advanced way")
+        mode = "advanced"
+    else:
+        print ("You have selected the Easy way")
+        mode = "easy"
+    return mode
+
+def print_tags(tags):
+    print ("Tag list:")
+    print ("{}\n\n".format(tags))
 
 def import_tag_selector(tags):
-    print (tags)
     selected_tags = []
+
     for tag in tags:
         answer = ""
         answer = input("Do you would to import games under tag \"{}\"? (No to cancel)\n".format(tag))
@@ -85,22 +106,58 @@ def select_games (tags, roms):
             selected_roms.append(rom)
     return selected_roms
 
+def check_params():
+    if (len(sys.argv) != 3):
+        print ("romselector.py: missing file\nusage python3 romselector.py originfolder destinationfolder\nEnter the origin and the destination folder.\n\n")
+        exit()
+
+def unique_selector(tags, roms):
+    selected_roms = []
+    limited_countries = []
+    print ("Unique selector")
+    #print ("Tag list {} roms list {}".format(tags,roms))
+    for rom in roms:
+        if rom_has_tags(['Europe', 'World', 'USA, Europe'], rom) & (len(rom) == 3):  
+            selected_roms.append(rom)
+    for rom in roms:
+        #print (rom)
+        if (not rom_has_tags(['Europe', 'World', 'USA, Europe'], rom)) & (len(rom) == 3):
+            if (rom[2] not in limited_countries):
+                limited_countries.append(rom[2])
+    
+    print ("We have added all the unique games with Europe, World and \"USA, Europe\" Versions ")
+    print ("There are some other unique games that maybe you would add too. The pending countries are the following:")
+    print (limited_countries)
+
+
+    # for rom in selected_roms:
+    #     print ("ROM: {} and lenght {}".format(rom,len(rom)))
+
+
+
 def main():
-    message_welcome()
-    roms_dir = read_params()
+    check_params()
+    welcome_message()
+    roms_dir, dest_dir = read_params()
     roms_list = list_files(roms_dir)
     splitted_roms = trim_files(roms_list)
     tag_list = list_tags(splitted_roms)
     count_games_tags(tag_list,splitted_roms)
-    selected_tags = import_tag_selector(tag_list)
-    selected_games = select_games(selected_tags, splitted_roms)
+    print_tags(tag_list)
+    mode=choose_selection_mode()
+    if (mode == 'easy'):
+        unique_selector(tag_list,splitted_roms)
+        exit()
+    else:
+        selected_tags = import_tag_selector(tag_list)
+        selected_games = select_games(selected_tags, splitted_roms)
+        exit()
     print("################# Lista de juegos añadidos #################")
     for rom in selected_games:
         print ("## {}".format(rom[1]))
     print("#############################################################")
     print ("De un total de {} juegos has añadido {})".format(len(roms_list), len(selected_games)))
-    #To DO:
-    #ADD THE POSSIBILITY TO SELECT "UNIQUE NON ADDED GAMES (Order EUR / USA / JAP / OTHERS)"
+
 
 
 if __name__== "__main__":
